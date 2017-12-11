cb9659543a9f2c2fae4108f1357404a84a2c9557
jharris0 <jrharris@gmail.com>
2017-12-10 18:30:56 -0700

    Reformatted code and added comments

--- build_ngrams_test_set.R
+++ build_ngrams_test_set.R
@@ -1,26 +1,29 @@
-# tokenize test set for 6-grams
+# Tokenize test set for 7-grams
 library(dplyr)
 library(tidyr)
 
-load(file = "corpus_test.Rdata")
-
 tokenizeNgrams <- function(x, level) {
-  library(tidytext)
-  ngrams <- x %>%
-    unnest_tokens(sentence, text, token = "sentences") %>%
-    unnest_tokens(ngram, sentence, token = "ngrams", n = level)
-  return(ngrams)
+        library(tidytext)
+        ngrams <- x %>%
+                unnest_tokens(sentence, text, token = "sentences") %>%
+                unnest_tokens(ngram, sentence, token = "ngrams", n = level)
+        return(ngrams)
 }
 
-corpus_df <- corpus_test %>%
-  mutate(linenumber = row_number()) %>%
-  select(linenumber, text)
+load(file = "corpus_test_clean.Rdata")
 
-corpus_sixgrams <- tokenizeNgrams(corpus_df, 6)
+# Prep data for tokenizing using tidytext package
+corpus_df <- corpus_test_clean %>%
+        mutate(linenumber = row_number()) %>%
+        select(linenumber, text)
 
-sixgrams_test_set <- corpus_sixgrams %>%
-  separate(ngram, c("word1", "word2", "word3", "word4", "word5", "word6"), sep = " ") %>%
-  mutate(word1_2_3_4_5 = paste(word1, word2, word3, word4, word5)) %>%
-  select(word1_2_3_4_5, word6)
+# Do the tokenizing
+corpus_sevengrams <- tokenizeNgrams(corpus_df, 7)
 
-save(sixgrams_test_set, file = "sixgrams_test_set.Rdata")
+# Split into two columns: first 6 words, and 7th word
+sevengrams_test_set <- corpus_sevengrams %>%
+        separate(ngram, c("word1", "word2", "word3", "word4", "word5", "word6", "word7"), sep = " ") %>%
+        mutate(word1_2_3_4_5_6 = paste(word1, word2, word3, word4, word5, word6)) %>%
+        select(word1_2_3_4_5_6, word7)
+
+save(sevengrams_test_set, file = "sevengrams_test_set.Rdata")
