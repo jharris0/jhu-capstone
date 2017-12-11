c5063c4e00dd9a0c8087cfcb599ea4a34b8101d8
jharris0 <jrharris@gmail.com>
2017-12-10 18:26:08 -0700

    Reformat code and add comments

--- build_ngram_tables.R
+++ build_ngram_tables.R
@@ -1,47 +1,48 @@
-# tokenize the corpus
+# Tokenize the training data and build ngram tables of most frequent ngrams.
+# Note: On a machine with 16 GB of RAM, I needed to run this script in segments,
+# i.e. I couldn't just source it and walk away. Generally, I would run each
+# ngram tokenizing operation separately, and then run each table building
+# operation separately.
+
 library(dplyr)
+library(tidytext)
 library(tidyr)
 library(data.table)
 
-# setwd("C:/Users/Jesse/gitrepos/jhu-capstone")
-
-# load(file = "corpus_train_clean.Rdata")
-
 tokenizeNgrams <- function(x, level) {
-  library(tidytext)
-  ngrams <- x %>%
-    unnest_tokens(sentence, text, token = "sentences") %>%
-    unnest_tokens(ngram, sentence, token = "ngrams", n = level)
-  return(ngrams)
+        library(tidytext)
+        ngrams <- x %>%
+                unnest_tokens(sentence, text, token = "sentences") %>%
+                unnest_tokens(ngram, sentence, token = "ngrams", n = level)
+        return(ngrams)
 }
 
-corpus_df <- corpus_train_clean %>%
-  mutate(linenumber = row_number()) %>%
-  select(linenumber, text)
+load(file = "corpus_train_clean.Rdata")
 
+# Prep data for tokenizing using tidytext package
+corpus_df <- corpus_train_clean %>%
+        mutate(linenumber = row_number()) %>%
+        select(linenumber, text)
+
+# Tokenize each ngram level
 corpus_bigrams <- tokenizeNgrams(corpus_df, 2)
 corpus_trigrams <- tokenizeNgrams(corpus_df, 3)
 corpus_fourgrams <- tokenizeNgrams(corpus_df, 4)
 corpus_fivegrams <- tokenizeNgrams(corpus_df, 5)
 
+# I recommend saving the results of the tokenizing process as it takes a while
+# to run
 save(corpus_bigrams, file = "corpus_bigrams.Rdata")
 save(corpus_trigrams, file = "corpus_trigrams.Rdata")
 save(corpus_fourgrams, file = "corpus_fourgrams.Rdata")
 save(corpus_fivegrams, file = "corpus_fivegrams.Rdata")
 
-# load("corpus_bigrams.Rdata")
-# load("corpus_trigrams.Rdata")
-# load("corpus_fourgrams.Rdata")
-# load("corpus_fivegrams.Rdata")
-
-library(tidyr) # reloading to overcome "could not find function 'separate' error
-
-# Bigrams
+# Build ngram tables, starting with bigrams
 bi_1 <- corpus_bigrams %>%
-  separate(ngram, c("word1", "word2"), sep = " ")
+        separate(ngram, c("word1", "word2"), sep = " ")
 setDT(bi_1)
 setkey(bi_1, word1)
-bi_2 <- bi_1[,.N, by = word1][order(-N)][1:75000]
+bi_2 <- bi_1[,.N, by = word1][order(-N)][1:75000] # Top 75000 bigrams
 bi_3 <- semi_join(bi_1, bi_2, by = "word1")
 setDT(bi_3)
 setkey(bi_3, word1)
@@ -52,21 +53,21 @@ bi_5 <- bi_4[N > 1]
 bi_6 <- list()
 
 for(i in unique(bi_5$word1)) {
-  dt_tmp <- bi_5[word1 == i]
-  dt_tmp <- dt_tmp[order(-prob)]
-  tmp <- dt_tmp[1:4,.(word1, word2, N2, prob)]
-  bi_6[[i]] <- tmp
+        dt_tmp <- bi_5[word1 == i]
+        dt_tmp <- dt_tmp[order(-prob)]
+        tmp <- dt_tmp[1:4,.(word1, word2, N2, prob)] # Top 4 3rd words for each bigram
+        bi_6[[i]] <- tmp
 }
 
 bigrams_final <- bind_rows(bi_6) %>% select(word1, word2, N2, prob) %>%
-  arrange(desc(N2), word1, desc(prob))
+        arrange(desc(N2), word1, desc(prob))
 bigrams_final <- bigrams_final[complete.cases(bigrams_final),]
 save(bigrams_final, file = "bigrams_final.Rdata")
 
 # Trigrams
 tri_1 <- corpus_trigrams %>%
-  separate(ngram, c("word1", "word2", "word3"), sep = " ") %>%
-  mutate(word1_2 = paste(word1, word2)) %>% select(word1_2, word3)
+        separate(ngram, c("word1", "word2", "word3"), sep = " ") %>%
+        mutate(word1_2 = paste(word1, word2)) %>% select(word1_2, word3)
 setDT(tri_1)
 setkey(tri_1, word1_2)
 tri_2 <- tri_1[,.N, by = word1_2][order(-N)][1:75000]
@@ -80,22 +81,22 @@ tri_5 <- tri_4[N > 1]
 tri_6 <- list()
 
 for(i in unique(tri_5$word1_2)) {
-  dt_tmp <- tri_5[word1_2 == i]
-  dt_tmp <- dt_tmp[order(-prob)]
-  tmp <- dt_tmp[1:4,.(word1_2, word3, N2, prob)]
-  tri_6[[i]] <- tmp
+        dt_tmp <- tri_5[word1_2 == i]
+        dt_tmp <- dt_tmp[order(-prob)]
+        tmp <- dt_tmp[1:4,.(word1_2, word3, N2, prob)]
+        tri_6[[i]] <- tmp
 }
 
 trigrams_final <- bind_rows(tri_6) %>% select(word1_2, word3, N2, prob) %>%
-  arrange(desc(N2), word1_2, desc(prob))
+        arrange(desc(N2), word1_2, desc(prob))
 trigrams_final <- trigrams_final[complete.cases(trigrams_final),]
 save(trigrams_final, file = "trigrams_final.Rdata")
 
 # Fourgrams
 four_1 <- corpus_fourgrams %>%
-  separate(ngram, c("word1", "word2", "word3", "word4"), sep = " ") %>%
-  mutate(word1_2_3 = paste(word1, word2, word3)) %>%
-  select(word1_2_3, word4)
+        separate(ngram, c("word1", "word2", "word3", "word4"), sep = " ") %>%
+        mutate(word1_2_3 = paste(word1, word2, word3)) %>%
+        select(word1_2_3, word4)
 setDT(four_1)
 setkey(four_1, word1_2_3)
 four_2 <- four_1[,.N, by = word1_2_3][order(-N)][1:75000]
@@ -109,23 +110,22 @@ four_5 <- four_4[N > 1]
 four_6 <- list()
 
 for(i in unique(four_5$word1_2_3)) {
-  dt_tmp <- four_5[word1_2_3 == i]
-  dt_tmp <- dt_tmp[order(-prob)]
-  tmp <- dt_tmp[1:4,.(word1_2_3, word4, N2, prob)]
-  four_6[[i]] <- tmp
+        dt_tmp <- four_5[word1_2_3 == i]
+        dt_tmp <- dt_tmp[order(-prob)]
+        tmp <- dt_tmp[1:4,.(word1_2_3, word4, N2, prob)]
+        four_6[[i]] <- tmp
 }
 
 fourgrams_final <- bind_rows(four_6) %>% select(word1_2_3, word4, N2, prob) %>%
-  arrange(desc(N2), word1_2_3, desc(prob))
+        arrange(desc(N2), word1_2_3, desc(prob))
 fourgrams_final <- fourgrams_final[complete.cases(fourgrams_final),]
-
 save(fourgrams_final, file = "fourgrams_final.Rdata")
 
 # Fivegrams
 five_1 <- corpus_fivegrams %>%
-  separate(ngram, c("word1", "word2", "word3", "word4", "word5"), sep = " ") %>%
-  mutate(word1_2_3_4 = paste(word1, word2, word3, word4)) %>%
-  select(word1_2_3_4, word5)
+        separate(ngram, c("word1", "word2", "word3", "word4", "word5"), sep = " ") %>%
+        mutate(word1_2_3_4 = paste(word1, word2, word3, word4)) %>%
+        select(word1_2_3_4, word5)
 setDT(five_1)
 setkey(five_1, word1_2_3_4)
 five_2 <- five_1[,.N, by = word1_2_3_4][order(-N)][1:75000]
@@ -139,13 +139,13 @@ five_5 <- five_4[N > 1]
 five_6 <- list()
 
 for(i in unique(five_5$word1_2_3_4)) {
-  dt_tmp <- five_5[word1_2_3_4 == i]
-  dt_tmp <- dt_tmp[order(-prob)]
-  tmp <- dt_tmp[1:4,.(word1_2_3_4, word5, N2, prob)]
-  five_6[[i]] <- tmp
+        dt_tmp <- five_5[word1_2_3_4 == i]
+        dt_tmp <- dt_tmp[order(-prob)]
+        tmp <- dt_tmp[1:4,.(word1_2_3_4, word5, N2, prob)]
+        five_6[[i]] <- tmp
 }
 
 fivegrams_final <- bind_rows(five_6) %>% select(word1_2_3_4, word5, N2, prob) %>%
-  arrange(desc(N2), word1_2_3_4, desc(prob))
+        arrange(desc(N2), word1_2_3_4, desc(prob))
 fivegrams_final <- fivegrams_final[complete.cases(fivegrams_final),]
 save(fivegrams_final, file = "fivegrams_final.Rdata")
